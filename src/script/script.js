8c4e78c0c2818188310016ab6c524f2880918ed5
depstor <depstor90@gmail.com>
2025-09-19 15:31:41 +0300

    prev-def

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,9 @@
 import '/src/sass/style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form')
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+    })
+})
